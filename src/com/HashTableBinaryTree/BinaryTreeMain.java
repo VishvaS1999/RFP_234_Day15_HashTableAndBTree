c597fadbffd0965c9a59ff11e31725ce28945d98
VishvaS1999 <gvishva2601@gmail.com>
2023-01-10 23:04:01 +0530

    Create Binary Tree checkall are add with Using Size Method

--- src/com/HashTableBinaryTree/BinaryTreeMain.java
+++ src/com/HashTableBinaryTree/BinaryTreeMain.java
@@ -83,6 +83,16 @@ public class BinaryTreeMain
         root = b1.insert(root, 70);
         root = b1.insert(root, 30);
         root = b1.insert(root, 56);
+        root = b1.insert(root, 22);
+        root = b1.insert(root, 40);
+        root = b1.insert(root, 60);
+        root = b1.insert(root, 3);
+        root = b1.insert(root, 63);
+        root = b1.insert(root, 65);
+        root = b1.insert(root, 95);
+        root = b1.insert(root, 6);
+        root = b1.insert(root, 11);
+        root = b1.insert(root, 67);
 
         b1.print(root);
     }
