1e4e1e8b2cfaae269a9bd3d371a44e0c75def0a6
VishvaS1999 <gvishva2601@gmail.com>
2023-01-10 22:49:09 +0530

    Welcome Hash Table And Binary Tree

---
+++ src/com/HashTableBinaryTree/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package com.HashTableBinaryTree;
+
+public class WelcomeMessage
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome Hash Table And Binary Tree");
+    }
+}
